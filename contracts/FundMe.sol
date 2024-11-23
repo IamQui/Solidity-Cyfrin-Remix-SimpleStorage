72a5f736f391bc7fd2fb19a59e8f6aa68f590584
IamQui <iamquicode@gmail.com>
2024-11-22 20:23:25 -0600

    getPrice

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -11,14 +11,18 @@ contract FundMe {
         require(msg.value >= minimumUSD, "Didn't send enough ETH");
     }
 
-    function getPrice() public {
-
+    function getPrice() public view returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        return uint256(price * 1e10); 
     }
+    // Sepolia ETH/USD => 0x694AA1769357215DE4FAC081bf1f309aDC325306
 
     function getConversionRate() public {}
 
-    // Sepolia ETH/USD => 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        
+    function getVersion() public view returns(uint256){
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
 
     //function withdraw() public {}
 }
