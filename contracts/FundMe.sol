0af9bda21a80097bbeb54e2c840c9e9b1cdc630b
IamQui <iamquicode@gmail.com>
2024-11-22 20:15:03 -0600

    Aggregator-import

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
 
     uint256 public minimumUSD = 5;
@@ -10,7 +12,7 @@ contract FundMe {
     }
 
     function getPrice() public {
-        
+
     }
 
     function getConversionRate() public {}
